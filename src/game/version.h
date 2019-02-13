53f2042187430e52d8edee57f2820eb24972a533
Jordy Ruiz <jordy.ruiz@univ-lille.fr>
2019-02-13 17:56:00 +0100

    Updated GAME_VERSION

--- src/game/version.h
+++ src/game/version.h
@@ -3,11 +3,11 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include <generated/nethash.cpp>
-#define GAME_VERSION "0.7.2, Race " RACE_VERSION
+#define GAME_VERSION "0.7.2, DDR " DDRACE_VERSION " (Race " RACE_VERSION ")"
 //#define GAME_NETVERSION "0.7 " GAME_NETVERSION_HASH
 #define GAME_NETVERSION "0.7 802f1be60a05665f"
 #define RACE_VERSION "4.0-dev"
-#define DDRACE_VERSION "1.0.1"
+#define DDRACE_VERSION "1.1"
 #define CLIENT_VERSION 0x0702
 static const char GAME_RELEASE_VERSION[8] = "0.7.2";
 #endif
